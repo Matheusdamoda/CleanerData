e66e5824def62a5b502c71c0d6b9646b5351ebb7
aMathyzin <Matheusdamoda@protonmail.com>
2024-03-31 01:22:29 -0300

    Create codejunk.bat

---
+++ codejunk.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 >nul
+echo Limpando arquivos temporários...
+
+:: Limpando pasta temporária do sistema
+echo Limpando pasta temporária do sistema...
+RD /S /Q %SystemRoot%\Temp 2>nul
+MD %SystemRoot%\Temp
+
+:: Limpando a variável de ambiente TEMP
+echo Limpando a variável de ambiente TEMP...
+RD /S /Q %TEMP% 2>nul
+MD %TEMP%
+
+:: Limpando arquivos de log
+echo Limpando arquivos de log...
+del /Q /F *.log
+
+echo Limpeza concluída.
+pause
