837b1ea61fa77d2951db1d4a3265bbd86a11d746
aMathyzin <Matheusdamoda@protonmail.com>
2024-04-01 01:05:03 -0300

    Update restore.bat

--- restore.bat
+++ restore.bat
@@ -1,7 +1,9 @@
 @echo off
 set "w=[97m" :: Branco
 set "r=[0m"
-wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "aMathyzin Cleaner", 100, 7
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "SystemRestorePointCreationFrequency" /t REG_DWORD /d 1 /f >nul 2>&1
+powershell -ExecutionPolicy Unrestricted -NoProfile Enable-ComputerRestore -Drive 'C:\', 'D:\', 'E:\', 'F:\', 'G:\' >nul 2>&1
+powershell -ExecutionPolicy Unrestricted -NoProfile Checkpoint-Computer -Description ' aMathyzin Cleaner' >nul 2>&1
 echo.
 echo %w% Ponto criado com sucesso. Nome: aMathyzin Cleaner.%r%
 pause
